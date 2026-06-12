3cc35dc60f2f781c420248f826647d5a08a1596c
Bruhbro22 <donmadhu411@gmail.com>
2026-06-12 12:32:11 -0500

    Add files via upload

---
+++ activity20.py
@@ -0,0 +1,11 @@
+a=10
+b=-10
+c=0
+if a>0 and b>0:
+    print("Both the numbers are positive")
+else:
+   print("Atleast one number will be negative")
+if a>0 or b>0:
+    print("One of the number is positive")
+else:
+    print("Both numbeers are negative")
